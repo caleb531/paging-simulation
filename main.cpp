f126235215d9321d5af39cd989d266eac58abcc2
Caleb Evans <caleb@calebevans.me>
2017-04-20 17:55:11 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ * CS 433 Programming Assignment 5: Memory Paging Simulation
+ * Caleb Evans, Jonathan Tapia
+ */
+#include <iostream>
+using namespace std;
+
+// Print the basic program information
+void printProgramHeader() {
+	cout << "CS 433 Paging Simulation" << endl;
+	cout << "Authors: Caleb Evans, Jonathan Tapia" << endl;
+	cout << endl;
+}
+
+int main() {
+
+	printProgramHeader();
+
+	return 0;
+
+}

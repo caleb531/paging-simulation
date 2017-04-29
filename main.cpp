5f6c16d323834f436b809690d56d69ffbfbc2d10
Caleb Evans <caleb@calebevans.me>
2017-04-29 11:12:48 -0700

    Perform proper type-checking on inputs
    atoi() does not perform type-checking. See
    <http://www.eternallyconfuzzled.com/arts/jsw_art_atoi.aspx>

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
  */
 #include <iostream>
 #include <cstdlib>
+#include <sstream>
 #include <fstream>
 using namespace std;
 
@@ -22,26 +23,38 @@ bool isPowerOfTwo(int n) {
 	return ((n & (n - 1)) == 0);
 }
 
+// Return the given string argument as an integer
+int getIntArg(char* strArg, const char* argLabel) {
+	if (!strArg) {
+		cerr << "Missing argument for " << argLabel << endl;
+		exit(1);
+	}
+	// Read string argument into stream and then read stream into integer to
+	// perform conversion
+	istringstream stream(strArg);
+	int intArg;
+	if (stream >> intArg) {
+		return intArg;
+	} else {
+		cerr << "Invalid " << argLabel << ": " << strArg << endl;
+		exit(1);
+	}
+}
+
 int main(int argc, char* argv[]) {
 	int pageSize, physicalMemorySize;
 
 	printProgramHeader();
 
-	if (argc != 3) {
-		cout << "Usage: " << argv[0] << " Page_Size Physical_Memory_Size"<< endl;
-		return 1;
-	}
-
-	// Note: atoi() doens't check for valid input
-	pageSize = atoi(argv[1]);
-	physicalMemorySize = atoi(argv[2]);
+	pageSize = getIntArg(argv[1], "page size");
+	physicalMemorySize = getIntArg(argv[2], "physical memory size");
 
 	if (pageSize < 256 || pageSize > 8192) {
-		cout << "Page_Size must be between 256 and 8192 inclusive" << endl;
+		cout << "Page size must be between 256 and 8192 inclusive" << endl;
 		return 1;
 	}
 	else if (!isPowerOfTwo(pageSize) || !isPowerOfTwo(physicalMemorySize)) {
-		cout << "Page_Size and Physical_Memory_Size must be powers of 2" << endl;
+		cout << "Page size and physical memory size must be powers of 2" << endl;
 		return 1;
 	}
 

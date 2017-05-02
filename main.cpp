d70ee5d8d34099143b492d5c9fddb957fe869e0e
Caleb Evans <caleb@calebevans.me>
2017-05-01 17:14:55 -0700

    Output message indicating replacer is running

--- main.cpp
+++ main.cpp
@@ -133,6 +133,7 @@ int main(int argc, char* argv[]) {
 
 	Replacer* replacer = promptForReplacer(pageTable, numFrames);
 
+	cout << "Running..." << endl;
 	int memRef;
 	while (refFile >> memRef) {
 
